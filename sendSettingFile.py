0e6eb1bef77851a6547cfcede738b086ae1260c9
AcCap <acapelli@alaska.edu>
2025-07-21 19:46:21 -0800

    Script for uploading LEMsettings file works now

--- sendSettingFile.py
+++ sendSettingFile.py
@@ -11,8 +11,8 @@ import zlib
 import argparse
 
 def send_file(port, baudrate, file_path):
-    with serial.Serial(port, baudrate, timeout=1) as ser:
-        time.sleep(2)  # Allow Arduino reset
+    with serial.Serial(port, baudrate, timeout=1 ) as ser:   # for avoiding reset of Arduino at start of serial: dsrdtr=True 
+        time.sleep(5)  # Allow Arduino reset
 
         with open(file_path, 'rb') as file:
             data = file.read()
@@ -22,21 +22,50 @@ def send_file(port, baudrate, file_path):
         print(f"Sending '{file_path}'")
         print(f"File size: {size} bytes, CRC32: {crc:#010x}")
 
+        # Empty serial buffer
+        ser.reset_input_buffer()
+        
+        # if ser.in_waiting > 0:
+           # ser.read(ser.in_waiting)
+
         # Send start command
-        ser.write(b'UPLOAD_SETFILE\n')
-        time.sleep(0.5)
+        # ser.write(b'UPLOAD_SETFILE\n')
+        # print("COM:--UPLOAD_SETFILE--")
+        # time.sleep(1)
 
-        # Send header: "size,crc"
-        header = f"{size},{crc}\n".encode()
-        ser.write(header)
-
+        # # Send header: "size,crc"
+        # ser.write("smalline\n".encode())
+        # ser.write(b'smalline\n')
+        # header = f"FSIZE:{size},{crc}\n".encode()
+        # ser.write(header)
+        # ser.write('\n'.encode('ascii'))
+        # print("COM:--",end='')
+        # print(header,end='')
+        # print("--")
+        # time.sleep(0.1)
+        
+        # Send start command with file size and CRC in one line
+        header = f"UPLOAD_SETFILE:{size},{crc}*\r\n"
+        print("COM:--",end='')
+        print(header,end='')
+        print("--")
+        ser.write(header.encode('ASCII'))
+        # ser.write(b'UPLOAD_SETFILE\n')
+        
+        time.sleep(1)
+        
         # Wait for READY
-        ack = ser.readline().decode().strip()
-        if ack != "READY":
+        ack=ser.readline().decode()
+        i=0
+        while( ack.strip() != "READY"):
+            i+=1
             print("Arduino not ready or error:", ack)
-            return
+            ack = ser.readline().decode()
+            if i>5:
+                return
 
         # Send data
+        print("Sending File")
         ser.write(data)
         print("File sent.")
 
